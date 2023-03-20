83233175fa7462c9ae2f00b4dfb9161a625074f5
“Jeke01” <josephikoheke1@gmail.com>
2023-03-20 15:41:01 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,26 @@
+/*
+ * File: 3-print_alphabets.c
+ * Auth: Jeke01
+ */
+
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet.
+ * main - Prints the alphabet in lowercase, and then in uppercase.
  *
- * Return always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	char alph[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXWY", int i;
-	
-	for (i = 0; i < 52; i++)
-	{
-		putchar(alp[i];
-	}
-	putchar("\n");
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
 	return (0);
 }
